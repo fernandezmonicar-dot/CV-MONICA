4464480ceadd784ed1193a970ebefca297d4f5d8
fernandezmonicar-dot <fernandezmonicar@gmail.com>
2026-06-22 09:52:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const tituloEducacion = document.querySelector('.titulo-educacion');
+const educacionLista = document.querySelector('.educacion-lista');
+const tituloHabilidades = document.querySelector('.titulo-habilidades');
+const habilidadesLista = document.querySelector('.habilidades-lista');
+const Datos = document.querySelector('.Datos');
+const DatosLista = document.querySelector('.Datos-lista');
+const tituloExperiencia = document.querySelector('.titulo-experiencia');
+const ExperienciaLista = document.querySelector('.experiencia-lista');
+const botonesEscuelas = document.querySelectorAll('.Escuelas');
+
+tituloEducacion.addEventListener('click', function() {
+    tituloEducacion.classList.toggle('oculto');
+});
+
+tituloHabilidades.addEventListener('click', function(){
+    tituloHabilidades.classList.toggle('oculto');
+});
+
+Datos.addEventListener('click', function() {
+    Datos.classList.toggle('oculto');
+});
+
+tituloExperiencia.addEventListener('click', function(){
+    tituloExperiencia.classList.toggle('oculto');
+});
+
+botonesEscuelas.forEach(function(boton) {
+    boton.addEventListener('click', function() {
+        const renglon = boton.parentElement;
+        const contenedor = renglon.querySelector('.contenedor-items');
+        
+        if (contenedor) {
+            contenedor.classList.toggle('oculto');
+        }
+    });
+});
